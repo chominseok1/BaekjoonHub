0b2befdc03b60a9702c824a75eee30587a15e8b8
chominseok1 <129127555+chominseok1@users.noreply.github.com>
2023-09-14 11:41:23 +0900

    [Bronze V] Title: 개수 세기, Time: 128 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,32 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.StringTokenizer;
-public class Main
-{
-    public static void main(String args[]) throws IOException
-    {
+import java.util.*;
+public class Main{
+    public static void main(String args[]) throws IOException{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		int num=Integer.parseInt(br.readLine()); // 배열의 크기
-		String s=br.readLine(); // 배열에 들어오는 숫자
-		StringTokenizer st=new StringTokenizer(s); // 들어오는 숫자들을 잘라서 배열의 넣으려고 생성
-		int n=Integer.parseInt(br.readLine()); // 세번째 들어오는 숫자
-		int arr[]=new int[num]; //
-		int index=0; // arr안에 세번째 들어오는 숫자의 갯수
-		for(int i=0;i<num;i++)
-		{
-			
-			arr[i]=Integer.parseInt(st.nextToken());
-		}
-		for(int count:arr)
-		{
-			if(count==n)
-			{
-				index++;
-			}
-		}
-		System.out.println(index);
+        int N=Integer.parseInt(br.readLine());
+        String s=br.readLine();
+        StringTokenizer st=new StringTokenizer(s); 
+        int t=Integer.parseInt(br.readLine()); // 세번째 숫자
+        int[] arr=new int[N];
+        int count=0;
+        for(int i=0;i<N;i++)
+        {
+            arr[i]=Integer.parseInt(st.nextToken());
+        }
+        for(int c:arr){
+            if(c==t)
+                count++;
+        }
+        System.out.println(count);
     }
 }
