c1316fdd4d63d3a2ab2560220a667120476830d6
chominseok1 <129127555+chominseok1@users.noreply.github.com>
2023-09-14 12:15:33 +0900

    [Bronze V] Title: X보다 작은 수, Time: 388 ms, Memory: 19388 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -1,28 +1,25 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.StringTokenizer;
-public class Main
-{
-    public static void main(String args[]) throws IOException
-    {
+import java.io.IOException;
+import java.util.*;
+public class Main{
+    public static void main(String args[]) throws IOException{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		String ss=br.readLine();
-		StringTokenizer st=new StringTokenizer(ss);
-		int N=Integer.parseInt(st.nextToken());
-		int X=Integer.parseInt(st.nextToken());
-		int arr[]=new int[N];
-		String num=br.readLine();
-		StringTokenizer st2=new StringTokenizer(num);
-		for(int i=0;i<N;i++)
-		{
-			arr[i]=Integer.parseInt(st2.nextToken());
-			if(arr[i]<X)
-			{
-				System.out.print(arr[i]+" ");
-			}
-		}
+        String s=br.readLine();
+        StringTokenizer st=new StringTokenizer(s);
+        int N=Integer.parseInt(st.nextToken());
+        int X=Integer.parseInt(st.nextToken());
+        int[] arr=new int[N];
+        String array=br.readLine();
+        StringTokenizer str=new StringTokenizer(array);
+        for(int i=0;i<N;i++)
+        {
+            arr[i]=Integer.parseInt(str.nextToken());
+        }
+        for(int a:arr)
+        {
+            if(a<X)
+                System.out.print(a+" ");
+        }
     }
 }
