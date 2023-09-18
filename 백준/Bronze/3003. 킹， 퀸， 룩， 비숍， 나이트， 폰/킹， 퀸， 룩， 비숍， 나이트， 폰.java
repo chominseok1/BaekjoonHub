409d963a44c5326ad677e8bf7abd91023e7c1e67
chominseok1 <129127555+chominseok1@users.noreply.github.com>
2023-09-18 17:38:23 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 192 ms, Memory: 16832 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -1,21 +1,25 @@
+import java.io.*;
 import java.util.*;
-public class Main
-{
-    public static void main(String args[])
-    {
-        Scanner scan=new Scanner(System.in);
-        int king=scan.nextInt();
-        int queen=scan.nextInt();
-        int look=scan.nextInt();
-        int bshop=scan.nextInt();
-        int knight=scan.nextInt();
-        int phone=scan.nextInt();
-        // 킹:1 퀸:1 룩:2 비숍:2 나이트:2 폰:8
-        System.out.print(1-king+" ");
-        System.out.print(1-queen+" ");
-        System.out.print(2-look+" ");
-        System.out.print(2-bshop+" ");
-        System.out.print(2-knight+" ");
-        System.out.print(8-phone);
+
+public class Main{
+    public static void main(String args[]) throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        String s=br.readLine();
+        // 11228
+        int k=1;
+        int q=1;
+        int l=2;
+        int b=2;
+        int n=2;
+        int p=8;
+        StringTokenizer st=new StringTokenizer(s);
+        int k2=Integer.parseInt(st.nextToken());
+        int q2=Integer.parseInt(st.nextToken());
+        int l2=Integer.parseInt(st.nextToken());
+        int b2=Integer.parseInt(st.nextToken());
+        int n2=Integer.parseInt(st.nextToken());
+        int p2=Integer.parseInt(st.nextToken());
+        System.out.print((k-k2)+" "+(q-q2)+" "+(l-l2)+" "+(b-b2)+" "+(n-n2)+" "+(p-p2));
+        
     }
 }
