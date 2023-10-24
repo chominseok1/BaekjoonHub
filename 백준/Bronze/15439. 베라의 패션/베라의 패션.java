e63ec4e48da062bf7763dc9cda79a60f45f55cd0
chominseok1 <129127555+chominseok1@users.noreply.github.com>
2023-10-24 16:23:51 +0900

    [Bronze IV] Title: 베라의 패션, Time: 124 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+public class Main{
+    public static void main(String args[]) throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int N=Integer.parseInt(br.readLine());
+        int answer=(int)Math.pow(N,2)-N;
+        System.out.println(answer);
+    }
+}
