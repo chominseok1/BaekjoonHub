285437b7ca1da17cc6b99806cb593dedaf561fed
chominseok1 <129127555+chominseok1@users.noreply.github.com>
2023-11-03 14:22:56 +0900

    [Bronze I] Title: 2007년, Time: 132 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/1924. 2007년/2007년.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+public class Main{
+    public static void main(String args[]) throws IOException{
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st=new StringTokenizer(br.readLine());
+        // 1월1일은 월요일 
+        int m=Integer.parseInt(st.nextToken());
+        int d=Integer.parseInt(st.nextToken());
+        int month[]={31,28,31,30,31,30,31,31,30,31,30,31};
+        String day[]={"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+        int total=d;
+        for(int i=0;i<m-1;i++){
+            total+=month[i];
+        }
+        System.out.println(day[total%7]);
+    }
+}
