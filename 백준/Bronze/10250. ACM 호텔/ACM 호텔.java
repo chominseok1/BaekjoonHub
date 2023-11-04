cd524e97218f8ce350d11ffb1ab9eb9897ed43d7
chominseok1 <129127555+chominseok1@users.noreply.github.com>
2023-11-04 14:33:22 +0900

    [Bronze III] Title: ACM 호텔, Time: 136 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+public class Main{
+    public static void main(String args[]) throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int a=Integer.parseInt(br.readLine());
+        for(int i=0;i<a;i++){
+            StringTokenizer st=new StringTokenizer(br.readLine());
+            int h=Integer.parseInt(st.nextToken());
+            int w=Integer.parseInt(st.nextToken());
+            int n=Integer.parseInt(st.nextToken());
+            
+            if(n%h==0){
+                System.out.println(h*100+(n/h));
+            }
+            else{
+                System.out.println((n%h)*100+(n/h+1));
+            }
+        }
+    }
+}
