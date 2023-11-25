cf668482c7492e5d1c19f9a1a3b6e315b5bc3736
chominseok1 <129127555+chominseok1@users.noreply.github.com>
2023-11-25 15:13:05 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 116 ms, Memory: 13832 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String args[]){
+        StringBuilder sb=new StringBuilder();
+        sb.append("비와이");
+        System.out.println(sb.toString());
+    }
+}
